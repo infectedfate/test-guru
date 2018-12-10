e9c1a27e96e15ea870b9209635b01f01d5141e00
Malinin Mikhail <infectedfate@gmail.com>
2018-12-10 12:49:15 +0300

    1

---
+++ db/migrate/20181210094336_add_default.rb
@@ -0,0 +1,6 @@
+class AddDefault < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :tests, :level, from: nil, to: 0
+    change_column_default :answers, :correct, from: nil, to: 0
+  end
+end
