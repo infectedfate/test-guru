b3ce6be30739b5b7222083f25a9e6707b231d273
Malinin Mikhail <infectedfate@gmail.com>
2018-12-14 12:42:45 +0300

    lesson 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
 end
+
+def passed_tests(level)
+  tests.by_level(level)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = Category.create([{ title: :embedded }, { title: :backend }])
+
+users = User.create(
+  [{ email: 'first@mail.com', username: :firstuser },
+   { email: 'second@mail.com', username: :seconduser }]
+)
+
+
+tests = Test.create(
+  [{ title: 'CSS', level: 1, category_id: categories[0].id,},
+  { title: 'HTML', level: 1, category_id: categories[1].id },
+  { title: 'Ruby', level: 2, category_id: categories[1].id },
+  { title: 'Python', level: 3, category_id: categories[1].id,}]
+)
+
+questions = Question.create(
+  [{ body: 'HTML question #1', test_id: tests[0].id },
+   { body: 'CSS question #1', test_id: tests[1].id },
+   { body: 'Ruby question #1', test_id: tests[2].id },
+   { body: 'Python question #1', test_id: tests[3].id }]
+)
