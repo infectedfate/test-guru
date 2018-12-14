1ee9c140b1711a0734d222963fb89022d2bb691c
Malinin Mikhail <infectedfate@gmail.com>
2018-12-14 12:44:48 +0300

    lesson 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-categories = Category.create([{ title: :embedded }, { title: :backend }])
+categories = Category.create([{ title: :frontend }, { title: :backend }])
 
 users = User.create(
   [{ email: 'first@mail.com', username: :firstuser },
@@ -7,10 +7,10 @@ users = User.create(
 
 
 tests = Test.create(
-  [{ title: 'CSS', level: 1, category_id: categories[0].id,},
+  [{ title: 'CSS', level: 1, category_id: categories[1].id,},
   { title: 'HTML', level: 1, category_id: categories[1].id },
-  { title: 'Ruby', level: 2, category_id: categories[1].id },
-  { title: 'Python', level: 3, category_id: categories[1].id,}]
+  { title: 'Ruby', level: 2, category_id: categories[2].id },
+  { title: 'Python', level: 3, category_id: categories[2].id,}]
 )
 
 questions = Question.create(
