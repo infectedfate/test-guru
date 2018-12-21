73acb6d827cbc97ad2a10c3d3b981a3f14a12c04
Malinin Mikhail <infectedfate@gmail.com>
2018-12-21 20:36:26 +0300

    4 lesson

--- db/migrate/20181221172240_create_test_passings.rb
+++ db/migrate/20181221172240_create_test_passings.rb
@@ -1,8 +1,6 @@
 class CreateTestPassings < ActiveRecord::Migration[5.2]
   def change
     create_table :test_passings do |t|
-      t.integer :user_id
-      t.integer :test_id
 
       t.timestamps
     end
