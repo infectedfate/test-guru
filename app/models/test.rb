ea5fdecf9da1a4556e6f30fbf0d4753f9147c38f
Malinin Mikhail <infectedfate@gmail.com>
2019-01-10 18:43:48 +0300

    rename function

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,7 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  def self.tests_in_category(title)
-    joins(:category).where(categories: { title: title}).order(id: :asc).pluck(:title)
+  def self.by_category(category_name)
+    joins(:category).where(categories: { title: category_name}).order(id: :asc).pluck(:title)
   end
 end
