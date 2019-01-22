24a415f6b26b9770216d38d4aa6634451e23211f
Malinin Mikhail <infectedfate@gmail.com>
2019-01-22 19:08:58 +0300

    6 lesson

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,17 @@
 class Question < ApplicationRecord
+  MAX_ANSWERS = 4
+
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validate :answers_count, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def answers_count
+    errors.add(:question) if question.answers.count >= MAX_ANSWERS
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,20 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  validates :title, presence: true, uniqueness: {
+    scope: :level
+  }
+  validates :level, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+
+  scope :by_level, ->(level) { where(level: level) }
+  scope :easy, -> { by_level(0..1) }
+  scope :medium, -> { by_level(2..4) }
+  scope :hard, -> { by_level(5..Float::INFINITY) }
+  scope :by_category, ->(category_name) { joins(:category).where(categories: { title: title }) }
+
   def self.by_category(category_name)
     joins(:category).where(categories: { title: category_name}).order(id: :asc).pluck(:title)
   end
