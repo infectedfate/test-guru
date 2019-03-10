f1e91f1e9b8d7b9f560217902c7f1535c20d40b6
Malinin Mikhail <infectedfate@gmail.com>
2019-03-11 00:08:40 +0300

    Lesson 8 added functions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -35,6 +35,12 @@ class TestsController < ApplicationController
     end
   end
 
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+    redirect_to tests_path
+  end
+
   private
 
   def test_params
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,8 @@
 module TestsHelper
+
+  TEST_LEVELS = { 0 => :elementary, 1 => :easy, 2 => :advanced }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hard
+  end
 end
