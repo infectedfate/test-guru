b6447c5202edbbc23df3f8b91f881aeec2965478
Malinin Mikhail <infectedfate@gmail.com>
2019-03-18 16:53:31 +0300

    add helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.new_record?
+      "Создать #{question.test.title} вопрос"
+    else
+      "Редактировать #{question.test.title} вопрос"
+    end
+  end
 end
