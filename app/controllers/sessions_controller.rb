3c6ad8f6c061df872e87fb4bfd93d9cfcc9a6ef0
Malinin Mikhail <infectedfate@gmail.com>
2019-04-12 09:26:18 +0300

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(mail: params[:mail])
 
     if user&.authenticate(params [:password])
-      session[:user_id] = user.:id
+      session[:user_id] = user.id
       redirect_to tests_path
     else
       flash.now[:alert] = 'Are yiu a Guru? Verify your Email and Passwor please'
