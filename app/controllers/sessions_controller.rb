7a1cd3a12dcddd9637f423911a080e650172565c
Malinin Mikhail <infectedfate@gmail.com>
2019-04-12 15:47:52 +0300

    fix auth problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(mail: params[:mail])
 
-    if user&.authenticate(params [:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to tests_path
     else
