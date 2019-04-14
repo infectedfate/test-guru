013fa78731988611508bce551546d27d2897c715
Malinin Mikhail <infectedfate@gmail.com>
2019-04-14 13:58:11 +0300

    fix answer controller problem

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,14 @@
 class Answer < ApplicationRecord
-  MAX_ANSWERS = 4
 
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
   validates :body, presence: true
-  validate :answers_count, on: :create
+  validate :validate_answers_count, on: :create
 
   private
 
-  def answers_count
-    errors.add(:question) if question.answers.count >= MAX_ANSWERS
+  def validate_answers_count
+    errors.add(:question) if question.answers.count >= 4
   end
 end
