bb7d3559d80461257b69bfee4c3dfe5f0f6766e4
Malinin Mikhail <infectedfate@gmail.com>
2019-04-16 08:55:48 +0300

    adding devise and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :mail, uniqueness: true, format: { with: /.+@.+\..+/i }
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'tests#index'
 
   get :signup, to: 'users#new'
@@ -19,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :test_passages, only: %i[show, update] do
+  resources :test_passages, only: %i[show update] do
     member do
       get :result
     end
