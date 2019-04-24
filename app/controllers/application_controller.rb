059f431f9b23107d8b55233d5c5ab9a749a08ae2
Malinin Mikhail <infectedfate@gmail.com>
2019-04-24 11:25:25 +0300

    Define locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_locale
+  
   def after_sign_in_path_for(user)
     user.is_a?(Admin) ? admin_tests_path : super
+  end
+
+  private
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+  
+
+  def set_locale
+    I18n.locale = I18n.lacale_avalible?.(params[:lang]) ? params[:lang] || I18n.default_locale
   end
 
   protected
