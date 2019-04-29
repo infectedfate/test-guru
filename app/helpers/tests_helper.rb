66e97e4ebd589b700a0456cd9cc315b19b4d3440
Malinin Mikhail <infectedfate@gmail.com>
2019-04-29 13:43:08 +0300

    Localization and delete unnecessary views

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,8 +1,12 @@
 module TestsHelper
 
-  TEST_LEVELS = { 0 => :elementary, 1 => :easy, 2 => :advanced }.freeze
+    def test_level(test)
+      case test.level
+      when 1 then t('.difficulty_level.easy')
+      when 2 then t('.difficulty_level.medium')
+      when 3 then t('.difficulty_level.hard')
+      else t('.difficulty_level.hero')
+      end
+    end
 
-  def test_level(test)
-    TEST_LEVELS[test.level] || :hard
-  end
 end
