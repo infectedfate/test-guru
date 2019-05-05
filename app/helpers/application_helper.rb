d501be2dc153dc1e1f52abb1ff5a22297af980fb
Malinin Mikhail <infectedfate@gmail.com>
2019-05-05 14:41:23 +0300

    flash type form alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,18 @@
 module ApplicationHelper
+  FLASH_TYPES = {
+    alert: 'warning',
+    notice: 'info'
+  }.freeze
+
   def flash_messages
     flash.map do |type, message|
       content_tag :p, message, class: "flash #{type}"
     end.join("\n").html_safe
   end 
+
+  private
+
+  def flash_type(type)
+    "alert alert-#{FLASH_TYPES[type.to_sym]}"
+  end
 end
