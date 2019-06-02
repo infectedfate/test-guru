2b9c7e01dbec6ad6cf1a42b07aab3e38fe36091e
Malinin Mikhail <infectedfate@gmail.com>
2019-06-02 18:05:57 +0300

    fix gists issue

--- db/migrate/20190602124310_rename_gists_table_column.rb
+++ db/migrate/20190602124310_rename_gists_table_column.rb
@@ -1,7 +1,7 @@
 class RenameGistsTableColumn < ActiveRecord::Migration[5.2]
   def change
     rename_column :gists, :question, :question_id
-    rename_column :gists, :user, :user_id
+    rename_column :gists, :user_email, :user_id
     #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end
