263e59eed821a1235a2cbff44a889751e0aca722
Malinin Mikhail <infectedfate@gmail.com>
2019-06-02 18:35:44 +0300

    have a problem with pstg in a prod, fix migrations

--- db/migrate/20190602124310_rename_gists_table_column.rb
+++ db/migrate/20190602124310_rename_gists_table_column.rb
@@ -1,7 +1,6 @@
 class RenameGistsTableColumn < ActiveRecord::Migration[5.2]
   def change
     rename_column :gists, :question, :question_id
-    rename_column :gists, :user_email, :user_id
     #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end
